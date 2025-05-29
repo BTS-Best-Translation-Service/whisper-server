6b533cb34a8ba50479a91893aad2ae08b980cd6c
cylin0201 <cylin0201@naver.com>
2025-05-29 18:32:27 +0900

    url 반 환 -> .srt 파일 자체 반환

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, BackgroundTasks
 from app.whisper_utils import transcribe_audio
 from app.translate import translate_segments_batch
 from app.srt_generator import generate_srt
 from app.s3_uploader import upload_to_s3
+from fastapi.responses import FileResponse
 import tempfile
 import shutil
 import os
@@ -11,7 +12,7 @@ app = FastAPI()
 
 
 @app.post("/process-audio")
-async def process_audio(file: UploadFile = File(...), video_id: str = Form(...)):
+async def process_audio(background_tasks: BackgroundTasks, file: UploadFile = File(...), video_id: str = Form(...)):
     print("#1. 원본 오디오 파일 임시 저장")
     with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[-1]) as tmp:
         shutil.copyfileobj(file.file, tmp)
@@ -29,8 +30,15 @@ async def process_audio(file: UploadFile = File(...), video_id: str = Form(...))
     print("#5. SRT 파일 생성")
     srt_path = generate_srt(translated, video_id)
 
-    print("#6. S3 업로드 후 삭제")
+    print("#6. S3 업로드 후에 로컬 파일 삭제")
     s3_url = upload_to_s3(srt_path, f"{video_id}.srt")
-    os.remove(s3_url)
+    background_tasks.add_task(os.remove, srt_path)
+    
 
-    return {"status": "success", "s3_url": s3_url}
+    print("#7. 파일 직접 응답으로 반환")
+    return FileResponse(
+        path=srt_path,
+        media_type="application/x-subrip", 
+        filename=f"{video_id}.srt"
+    )
+
